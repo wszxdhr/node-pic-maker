991c1eb7c263199b4e69c418f8839a9f32a9d41f
张潇 <wszxdhr@126.com>
2017-11-22 22:58:22 +0800

    优化模板渲染机制

--- modules/render/query2template.js
+++ modules/render/query2template.js
@@ -4,7 +4,11 @@ const sequelize = require('../sql/models/template')
 module.exports = async function (query) {
   return JSON.parse(await sequelize
     // 根据模板名称找到模板
-    .findOne({name: query.template})
+    .find({
+      where: {
+        name: query.template
+      }
+    })
     .then(res => {
       let template = res.template
       let args = JSON.parse(res.arguments)
@@ -12,10 +16,18 @@ module.exports = async function (query) {
       for (let arg of args) {
         // fixme: 如果template本身 或 参数中含有${img1}类似字样会出错 或 query参数中出现 '{', '}', '['等特殊字符时，未处理
         // 将字符串拆分为
-        let splitedArg = template.split('${' + arg + '}')
-        let insertStr = query[arg]
-        template = splitedArg.join(insertStr)
+        let matchReg = new RegExp(`\\$\\{${arg}([\\s]*\\|[^\\}]*?){0,1}\\}`)
+        let defaultValue = ((template
+          .match(matchReg) || [''])[0]
+          .match(/[\|][^}]*\}$/) || [''])[0]
+          .slice(1, -1)
+          .trim()
+        console.log('default')
+        console.log(query[arg])
+        let insertStr = query[arg] !== undefined ? query[arg] : defaultValue
+        template = template.replace(matchReg, insertStr)
       }
+      // console.log(template)
       return template
     }))
 }
